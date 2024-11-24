5433f09710c55fae839ff2143e7897bc8e56ad8d
Sohaib-Azhar010 <sohaibazhar04@gmail.com>
2024-11-24 16:48:28 +0500

    .

--- users/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-def home_view(request):
-    # Data to pass to the template
-    context = {'message': 'Welcome to my Django App!'}
-    return render(request, 'home.html', context)
